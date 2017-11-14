e929ea42be84991b398201303c40b24a52e1ef05
Sten-Oliver Salumaa <salumaa@ut.ee>
2017-11-14 17:28:59 +0100

    now saving pageranks and airport names to file. Added some comments and experimentally-used code (timing and counting iterations)

--- PageRank.py
+++ PageRank.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+import operator
+import time
 
 
 class PR:
@@ -9,22 +11,30 @@ class PR:
         self.precision = precision
 
     def compute_page_ranks(self):
+        # at first q is dict of airport codes as keys and 1/n as values
         q = {i: 1 / len(self.airports) for i in self.airports}
 
+        # iteration counter
+        k = 0
         while True:
+            # p is a copy of q so that changing p wouldn't change q
             p = dict(q)
             for destination in self.airports:
+                # apply pageRank
                 q[destination] = \
                     self.df \
                     * sum([weight * p[origin] for origin, weight in self.routes[destination].items()]) \
                     + (1 - self.df) / len(self.airports)
 
+            # check if iteration values already too similar
             if self.stop(p, q):
                 return PR.norm(q)
 
+            k += 1
+
     def stop(self, p, q):
         for k in p.keys():
-            if q[k] + self.precision > p[k] > q[k] - self.precision:
+            if q[k] + self.precision > p[k] and p[k] > q[k] - self.precision:
                 return True
         return False
 
@@ -79,15 +89,25 @@ class PR:
 
 
 def main():
-    with open("airports.txt", "r") as f:
+    with open("airports.txt", "r", encoding="utf8") as f:
         airports_as_str = f.readlines()
-    with open("routes.txt", "r") as f:
+    with open("routes.txt", "r", encoding="utf8") as f:
         routes_as_str = f.readlines()
 
-    pr = PR.create(airports_as_str, routes_as_str, 2 / 3, 1e-5)
+    pr = PR.create(airports_as_str, routes_as_str, 2/3, 1e-5)
+    # measuring pageRank elapsed time
+    start_time = time.time()
+    try_count = 10
+    for i in range(10):
+        ranks = (pr.compute_page_ranks())
+    print("Elapsed time: " + str((time.time() - start_time)/try_count))
+    # if sum of probabilities equals to one then the pageRank vector is done right
+    prob_sum = (sum(pr.compute_page_ranks().values()))
 
-    print(pr.compute_page_ranks())
-    print(sum(pr.compute_page_ranks().values()))
+    sorted_x = list(reversed(sorted(ranks.items(), key=operator.itemgetter(1))))
+    with open("output.txt", mode='w', encoding="utf8") as f:
+        # [print(str(element[1]) + ", " + pr.airports[str(element[0])]) for element in sorted_x]
+        [f.write(str(element[1]) + ", " + pr.airports[str(element[0])] + "\n") for element in sorted_x]
 
 
 if __name__ == "__main__":
