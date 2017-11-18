aeb2c86d58236c8f4185dc95a7a14e9f733b2334
grzesiu <grzspychala@gmail.com>
2017-11-18 13:58:21 +0100

    Take only routes of the airports present in the airports file

--- PageRank.py
+++ PageRank.py
@@ -1,6 +1,5 @@
-from collections import defaultdict
-import operator
 import time
+from collections import defaultdict
 
 
 class PR:
@@ -17,34 +16,27 @@ class PR:
         # iteration counter
         k = 0
         while True:
-            # p is a copy of q so that changing p wouldn't change q
+            k += 1
             p = dict(q)
             for destination in self.airports:
-                # apply pageRank
                 q[destination] = \
                     self.df \
                     * sum([weight * p[origin] for origin, weight in self.routes[destination].items()]) \
                     + (1 - self.df) / len(self.airports)
 
-            # check if iteration values already too similar
             if self.stop(p, q):
-                return PR.norm(q)
-
-            k += 1
+                return PR.norm(q), k
 
     def stop(self, p, q):
         for k in p.keys():
-            if q[k] + self.precision > p[k] and p[k] > q[k] - self.precision:
+            if q[k] + self.precision > p[k] > q[k] - self.precision:
                 return True
         return False
 
     @classmethod
     def create(cls, airports_as_str, routes_as_str, df, precision):
         airports = cls.read_airports(airports_as_str)
-        routes = cls.read_routes(routes_as_str)
-
-        airports = PR.add_missing(routes, airports, "NO_NAME")
-        routes = PR.add_missing(airports, routes, defaultdict(float))
+        routes = cls.read_routes(routes_as_str, airports)
 
         return cls(airports, routes, df, precision)
 
@@ -62,26 +54,23 @@ class PR:
         return airports
 
     @staticmethod
-    def read_routes(routes_as_str):
+    def read_routes(routes_as_str, airports):
         routes = defaultdict(lambda: defaultdict(float))
         out_degs = defaultdict(float)
         for line in routes_as_str:
             temp = line.split(',')
             origin = temp[2]
             destination = temp[4]
-            routes[destination][origin] += 1
-            out_degs[origin] += 1
+
+            if origin in airports and destination in airports:
+                routes[destination][origin] += 1
+                out_degs[origin] += 1
 
         for destination in routes:
             for origin in routes[destination]:
                 routes[destination][origin] /= out_degs[origin]
 
         return routes
-
-    @staticmethod
-    def add_missing(from_dict, to_dict, default):
-        missing = {k: default for k in set(from_dict) - set(to_dict)}
-        return {**to_dict, **missing}
 
     @staticmethod
     def norm(p):
@@ -94,20 +83,17 @@ def main():
     with open("routes.txt", "r", encoding="utf8") as f:
         routes_as_str = f.readlines()
 
-    pr = PR.create(airports_as_str, routes_as_str, 2/3, 1e-5)
-    # measuring pageRank elapsed time
-    start_time = time.time()
-    try_count = 10
-    for i in range(10):
-        ranks = (pr.compute_page_ranks())
-    print("Elapsed time: " + str((time.time() - start_time)/try_count))
-    # if sum of probabilities equals to one then the pageRank vector is done right
-    prob_sum = (sum(pr.compute_page_ranks().values()))
+    pr = PR.create(airports_as_str, routes_as_str, 0.8, 1e-20)
 
-    sorted_x = list(reversed(sorted(ranks.items(), key=operator.itemgetter(1))))
-    with open("output.txt", mode='w', encoding="utf8") as f:
-        # [print(str(element[1]) + ", " + pr.airports[str(element[0])]) for element in sorted_x]
-        [f.write(str(element[1]) + ", " + pr.airports[str(element[0])] + "\n") for element in sorted_x]
+    start_time = time.time()
+
+    result, k = pr.compute_page_ranks()
+
+    end_time = time.time()
+
+    print("Number of iterations:", k)
+    print("Time of calculating PageRanks:", end_time - start_time)
+    print(result)
 
 
 if __name__ == "__main__":
